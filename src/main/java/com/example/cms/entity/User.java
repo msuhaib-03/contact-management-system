9452ec8d361b6a9e56959191d68b3db886aac303
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2025-12-30 12:09:55 +0500

    Register Authentication & Login working

--- src/main/java/com/example/cms/entity/User.java
+++ src/main/java/com/example/cms/entity/User.java
@@ -33,8 +33,8 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
-    private String role = "USER"; // default role
+//    @Column(nullable = false)
+//    private String role = "USER"; // default role
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
