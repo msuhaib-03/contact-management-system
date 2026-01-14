e740170afe8d10401fa4810ebdfa002802997e3b
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-01-15 00:42:41 +0500

    checking the frontend and backend integration with database by showing user and contact detail

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -20,7 +20,8 @@ api.interceptors.response.use(
     (response) => response,
     (error) => {
         if (error.response?.status === 401) {
-            logout();
+            localStorage.clear();
+            //logout();
             window.location.href = "/login";
         }
         return Promise.reject(error);
