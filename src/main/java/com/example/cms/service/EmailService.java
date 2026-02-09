5a02154058e913f4a40ca0cfff210767ab759ed2
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-02-09 14:50:01 +0500

    Java mail service addition with SMTP

---
+++ src/main/java/com/example/cms/service/EmailService.java
@@ -0,0 +1,22 @@
+package com.example.cms.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendOtp(String to, String otp) {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(to);
+        msg.setSubject("Your Password Reset OTP");
+        msg.setText("Your OTP is: " + otp + "\nExpires in 5 minutes.");
+
+        mailSender.send(msg);
+    }
+}
